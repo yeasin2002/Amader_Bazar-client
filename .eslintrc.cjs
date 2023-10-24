de99b150d18ff7147a6a828278ab4717e7d1f245
yeasin-2002 <mdkawsarislam2002@gmail.com>
2023-10-24 13:20:47 +0600

    :zap: configured prettier,eslint, husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,29 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@tanstack/eslint-plugin-query/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "@tanstack/query"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@tanstack/eslint-plugin-query/recommended",
+        "plugin:prettier/recommended",
     ],
-    "@tanstack/query/exhaustive-deps": "error",
-    "@tanstack/query/prefer-query-object-syntax": "error",
-    "@tanstack/query/stable-query-client": "error",
-  },
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-refresh", "@tanstack/query", "perfectionist", "unicorn"],
+    rules: {
+        "unicorn/better-regex": "error",
+        "unicorn/…": "error",
+        "@tanstack/query/exhaustive-deps": "error",
+        "@tanstack/query/prefer-query-object-syntax": "error",
+        "@tanstack/query/stable-query-client": "error",
+        "perfectionist/sort-objects": [
+            "error",
+            { type: "natural", order: "asc" },
+        ],
+        "react-refresh/only-export-components": [
+            "warn",
+            { allowConstantExport: true },
+        ],
+    },
 };
