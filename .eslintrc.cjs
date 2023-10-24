8bd179a3f2d9ff2a6597f3dfbacb948ce202ae60
yeasin-2002 <mdkawsarislam2002@gmail.com>
2023-10-24 13:22:36 +0600

    :technologist: Configured Import Aliases and setup shadcn.ui

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,24 +6,13 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:react-hooks/recommended",
         "plugin:@tanstack/eslint-plugin-query/recommended",
-        "plugin:prettier/recommended",
     ],
-    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules"],
     parser: "@typescript-eslint/parser",
     plugins: ["react-refresh", "@tanstack/query", "perfectionist", "unicorn"],
     rules: {
-        "unicorn/better-regex": "error",
-        "unicorn/…": "error",
         "@tanstack/query/exhaustive-deps": "error",
         "@tanstack/query/prefer-query-object-syntax": "error",
         "@tanstack/query/stable-query-client": "error",
-        "perfectionist/sort-objects": [
-            "error",
-            { type: "natural", order: "asc" },
-        ],
-        "react-refresh/only-export-components": [
-            "warn",
-            { allowConstantExport: true },
-        ],
     },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            $: path.resolve(__dirname, "./"),
+            $src: path.resolve(__dirname, "./src/"),
+            $asserts: `${path.resolve(__dirname, "./src/asserts/")}`,
+            $hooks: `${path.resolve(__dirname, "./src/hooks/")}`,
+            $layout: `${path.resolve(__dirname, "./src/layout/")}`,
+            $pages: `${path.resolve(__dirname, "./src/pages/")}`,
+            $utils: `${path.resolve(__dirname, "./src/utils/")}`,
+            $data: `${path.resolve(__dirname, "./src/data")}`,
+            $store: `${path.resolve(__dirname, "./src/store/")}`,
+            $style: `${path.resolve(__dirname, "./src/style/")}`,
+            $components: `${path.resolve(__dirname, "./src/components/")}`,
+            $ui: `${path.resolve(__dirname, "./src/ui/")}`,
+        },
+    },
+});
