a11eb3a840d582ae0ba40c2e0e09fd0a9d262eac
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-11-19 01:39:37 +0600

    Removed Clerk/Auth

--- src/lib/exportEnv.ts
+++
@@ -1,2 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-export const clerkPubKey: string = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY
--- src/main.tsx
+++ src/main.tsx
@@ -8,26 +8,15 @@ import { AuthProvider } from "./context"
 import "./index.css"
 const queryClient = new QueryClient()
 
-//? clerk Authentication
-import { clerkPubKey } from "$lib"
-import { ClerkProvider } from "@clerk/clerk-react"
-import { toast } from "sonner"
-if (!clerkPubKey) {
-  console.log("Missing Publishable Key")
-  toast.error("Missing Publishable Key")
-}
-
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </QueryClientProvider>
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
